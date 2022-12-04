5d7ad99a16417f9e05796dfab087574d9bf3d576
msomu07 <94503844+msomu07@users.noreply.github.com>
2022-12-04 14:57:52 -0800

    updated css and error message as a modal

--- Wordsearch.js
+++ Wordsearch.js
@@ -201,10 +201,36 @@ function checkWord()
 	
 	if(!valid)
 	{
+		// Get the modal
+		var modal = document.getElementById("myModal");
+
+		// Get the button that opens the modal
+		var btn = document.getElementById("myBtn");
+
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks the button, open the modal 
+		btn.onclick = function() {
+		  modal.style.display = "block";
+		}
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+		  modal.style.display = "none";
+		}
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+		  if (event.target == modal) {
+			modal.style.display = "none";
+		  }
+		}
+		
 		document.getElementById("error-message").innerHTML += "ERROR -- PLEASE CHECK THAT" //errormessage
-		+ "<br>" + "- ALL VALUES HAVE BEEN ENTERED CORRECTLY" + "<br>" +
-		"- ALL VALUES ARE WITHIN THE GIVEN RANGE" + "<br>" +
-		"- START VALUE COMES *BEFORE* END VALUE";
+		+ "<br>" + "✧  ALL VALUES HAVE BEEN ENTERED CORRECTLY" + "<br>" +
+		"✧  ALL VALUES ARE WITHIN THE GIVEN RANGE" + "<br>" +
+		"✧  START VALUE COMES *BEFORE* END VALUE";
 		
 	} else {
 		let isCorrect = true;
